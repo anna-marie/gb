a24cda6701c198107a70b3548e4079a8bf85f89e
Anna-Marie <arkrahn@shaw.ca>
2014-07-08 14:19:43 -0700

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Guild Builder" }
+
   describe "Home page" do
 
     it "should have the content 'Guild Builder'" do
@@ -9,9 +11,14 @@ describe "Static pages" do
       expect(page).to have_content('Guild Builder')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Guild Builder | Home")
+      expect(page).to have_title("Guild Builder")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
@@ -24,7 +31,7 @@ describe "Static pages" do
 
     it "should have the title 'About'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Guild Builder | About")
+      expect(page).to have_title("#{base_title} | About")
     end
   end
 
@@ -37,7 +44,7 @@ describe "Static pages" do
 
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
-      expect(page).to have_title("Guild Builder | Contact")
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
